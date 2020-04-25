2ea09c2c8270fc54046d955e802b6223a896d0ea
caleb okogbe <okogbecaleb@gmail.com>
2020-04-25 12:26:59 +0100

    Improved corrections

--- CarInventoryAPP/Program.cs
+++ CarInventoryAPP/Program.cs
@@ -7,42 +7,84 @@ namespace CarInventoryAPP
     {
         static void Main(string[] args)
         {
-            Tokunbor tokunbor = new Tokunbor();
+            
 
-            
-            
+            List<Tokunbor> list = new List<Tokunbor>();
+
+       
 
             Console.WriteLine("Welcome to Caleb Okogbe's Cool “Tokunbo” Autos!");
-
-            while(true)
-            {
-                Console.Write("Enter command: ");
-                string command = Console.ReadLine();
-
-                if(command == "add")
+            Console.Write("Enter command add,list or quit: ");
+            string command = Console.ReadLine();
+            
+            if(command == "add")
+                {   
+               
+                while(command == "add")
                 {
-                    tokunbor.Catalog();
-                    
+                    Catalog();
                 }
                 if(command == "list")
                 {
-                        tokunbor.Lists();
-                        break;
-                    
+                    Lists();
                 }
-                if(command == "quit")
+            }
+            else if(command == "list")
+                {
+                        Console.Write("There are currently no cars in the catalog.");
+                }
+            else if(command == "quit")
                 {
                     Console.Write("Good bye!");
-                        break;
                     
                 }
-                if(command != "add")
+
+            
+            else if(command != "add" || command != "list" || command != "quit")
                 {   
                     Console.WriteLine($"Sorry, but {command} is not a valid command. Please try again.");
                 }
+
+            void Catalog()
+            {
+                
+                
+                Console.Write("Enter car make: ");
+                string Make = Console.ReadLine();
+
+                Console.Write("Enter car model: ");
+                string Model = Console.ReadLine();
+
+                Console.Write("Enter car year: ");
+                int Year = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter car price: ");
+                decimal Price = Convert.ToDecimal(Console.ReadLine());
+
+               
+
+                Tokunbor tokunbor = new Tokunbor(Make, Model, Year, Price);
+
+                
+                
+                list.Add(tokunbor);
+            
+                Console.Write("Enter command add,list or quit: ");
+                command = Console.ReadLine();
             }
 
-            
+            void Lists()
+            {
+                decimal totalinventory = 0;
+                int count = 0;
+                foreach (var item in list)
+            {   
+                count++;
+                totalinventory += item.Price;
+                Console.Write(item);
+            }
+            Console.Write($"=====================\nTotal Cars available: {count}\nTotal Inventory: {totalinventory:C}");
+            }
 
         }
     }
--- CarInventoryAPP/Tokunbor.cs
+++ CarInventoryAPP/Tokunbor.cs
@@ -5,49 +5,60 @@ namespace CarInventoryAPP
 {
     public class Tokunbor
     { 
-        public string make;
-        public string model;
-        public int year;
-        public decimal price;
-        public string result;
-        public decimal totalinventory;
-        List<string> list = new List<string>() {};
+        public string Make{get; set;}
+        public string Model{get; set;}
+        public int Year{get; set;}
+        public decimal Price{get; set;}
+
+        public Tokunbor(string make, string model, int year, decimal price)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Price = price;
+            
+        }
+        
+        public override string ToString()
+        {
+            return $"==================================\nMake: {Make}\nModel: {Model}\nYear: {Year}\nPrice: {Price:C}\n";
+        }
         
 
-        public void Catalog()
-        {
-            Console.Write("Enter car make: ");
-            make = Console.ReadLine();
+        // public void Catalog()
+        // {
+        //     Console.Write("Enter car make: ");
+        //     make = Console.ReadLine();
 
-            Console.Write("Enter car model: ");
-            model = Console.ReadLine();
+        //     Console.Write("Enter car model: ");
+        //     model = Console.ReadLine();
 
-            Console.Write("Enter car year: ");
-            year = Convert.ToInt32(Console.ReadLine());
+        //     Console.Write("Enter car year: ");
+        //     year = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter car price: ");
-            price = Convert.ToDecimal(Console.ReadLine());
+        //     Console.Write("Enter car price: ");
+        //     price = Convert.ToDecimal(Console.ReadLine());
 
-            totalinventory += price;
+        //     totalinventory += price;
 
             
-           result = $"==================================\nMake: {make}\nModel: {model}\nYear: {year}\nPrice: {price:C}\n";
-            list.Add(result);
-        }
+        //    result = $"==================================\nMake: {make}\nModel: {model}\nYear: {year}\nPrice: {price:C}\n";
+        //     list.Add(result);
+        // }
 
 
-        public void Lists()
-        {
-            foreach (var item in list)
-            {
-                if(list == null)
-                {
-                    Console.Write("There are currently no cars in the catalog.");
-                }else
-                Console.Write(item);
-            }
-            Console.Write($"Total Inventory: {totalinventory:C}");
-        }
+        // public void Lists()
+        // {
+        //     foreach (var item in list)
+        //     {
+        //         if(list == null)
+        //         {
+        //             Console.Write("There are currently no cars in the catalog.");
+        //         }else
+        //         Console.Write(item);
+        //     }
+        //     Console.Write($"Total Inventory: {totalinventory:C}");
+        // }
         
         
         
